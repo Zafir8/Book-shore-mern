82566a8ab1aa840853dd5aae284c395e63501a0a
Mohamed Zafir Sharaz <zafirsharaz@gmail.com>
2024-03-03 13:56:44 +0530

    all methods working

--- backend/index.js
+++ backend/index.js
@@ -67,6 +67,54 @@ app.get('/books/:id', async (request, response) => {
     }
 });
 
+// Route to update a book by ID
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Please fill in all required fields: title, author, publishYear',
+            });
+        }
+
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' });
+
+        }
+        return response.status(200).send({ message: 'Book updated successfully' });
+
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+});
+
+// Route to delete a book by ID
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' });
+        }
+    
+        return response.status(200).send({ message: 'Book deleted successfully' });
+
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+}
+);
+
+
 
 mongoose
     .connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
