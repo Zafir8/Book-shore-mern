cff60b5912dbc0f2d0f6fb4c2ea9edeae767ca0c
Mohamed Zafir Sharaz <zafirsharaz@gmail.com>
2024-03-04 23:14:18 +0530

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBook";
+import EditBook from "./pages/EditBook";
+import ShowBook from "./pages/ShowBook";
+import DeleteBook from "./pages/DeleteBook";
+
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/books" element={<Books />} />
-      <Route path="/books/:id" element={<Book />} />
-      <Route path="*" element={<NotFound />} />
-
+      <Route path="/create" element={<CreateBook />} />
+      <Route path="/edit/:id" element={<EditBook />} />
+      <Route path="/show/:id" element={<ShowBook />} />
+      <Route path="/delete/:id" element={<DeleteBook />} />
     </Routes>
    
   );
