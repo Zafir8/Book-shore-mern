5405dd66fe6c8a7fd5b021160cf930889a2bbe7b
Mohamed Zafir Sharaz <zafirsharaz@gmail.com>
2024-03-05 01:55:48 +0530

    updated to card viev

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,82 +7,62 @@ import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const Home = () => {
-    const [books, setBooks] = useState([]);
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-      setLoading(true);
-      axios
-        .get('http://localhost:5555/books')
-        .then((response) => {
-          setBooks(response.data.data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-          setLoading(false);
-        });
-    }, []);
-    return (<div className='p-4'>
-    <div className='flex justify-between items-center'>
-      <h1 className='text-3xl my-8'>Books List</h1>
-      <Link to='/books/create'>
-        <MdOutlineAddBox className='text-sky-800 text-4xl' />
-      </Link>
-    </div>
-    {loading ? (
-      <Spinner />
-    ) : (
-      <table className='w-full border-separate border-spacing-2'>
-        <thead>
-          <tr>
-            <th className='border border-slate-600 rounded-md'>No</th>
-            <th className='border border-slate-600 rounded-md'>Title</th>
-            <th className='border border-slate-600 rounded-md max-md:hidden'>
-              Author
-            </th>
-            <th className='border border-slate-600 rounded-md max-md:hidden'>
-              Publish Year
-            </th>
-            <th className='border border-slate-600 rounded-md'>Operations</th>
-          </tr>
-        </thead>
-        <tbody>
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('http://localhost:5555/books')
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Books List</h1>
+        <Link to='/books/create'>
+          <MdOutlineAddBox className='text-sky-800 text-4xl' />
+        </Link>
+      </div>
+
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
           {books.map((book, index) => (
-            <tr key={book._id} className='h-8'>
-              <td className='border border-slate-700 rounded-md text-center'>
-                {index + 1}
-              </td>
-              <td className='border border-slate-700 rounded-md text-center'>
-                {book.title}
-              </td>
-              <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-                {book.author}
-              </td>
-              <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-                {book.publishYear}
-              </td>
-              <td className='border border-slate-700 rounded-md text-center'>
-                <div className='flex justify-center gap-x-4'>
-                  <Link to={`/books/details/${book._id}`}>
-                    <BsInfoCircle className='text-2xl text-green-800' />
-                  </Link>
-                  <Link to={`/books/edit/${book._id}`}>
-                    <AiOutlineEdit className='text-2xl text-yellow-600' />
-                  </Link>
-                  <Link to={`/books/delete/${book._id}`}>
-                    <MdOutlineDelete className='text-2xl text-red-600' />
-                  </Link>
-                </div>
-              </td>
-            </tr>
+            <div
+              key={book._id}
+              className='bg-white rounded-md shadow-md p-4'
+            >
+              <p className='text-xl font-bold mb-2'>{book.title}</p>
+              <p className='text-gray-600 mb-2'>{`Author: ${book.author}`}</p>
+              <p className='text-gray-600 mb-2'>{`Publish Year: ${book.publishYear}`}</p>
+
+              <div className='flex justify-center gap-x-4'>
+                <Link to={`/books/details/${book._id}`}>
+                  <BsInfoCircle className='text-2xl text-green-800' />
+                </Link>
+                <Link to={`/books/edit/${book._id}`}>
+                  <AiOutlineEdit className='text-2xl text-yellow-600' />
+                </Link>
+                <Link to={`/books/delete/${book._id}`}>
+                  <MdOutlineDelete className='text-2xl text-red-600' />
+                </Link>
+              </div>
+            </div>
           ))}
-        </tbody>
-      </table>
-    )}
-  </div>
-);
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
-
-
