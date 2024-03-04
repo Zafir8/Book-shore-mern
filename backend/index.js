947963491e9e6f863a45257cb65a6d394762116c
Mohamed Zafir Sharaz <zafirsharaz@gmail.com>
2024-03-05 01:12:15 +0530

    home page and crud

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,16 @@ const app = express();
 app.use(express.json());
 
 // Middleware to allow cross-origin requests
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+app.use(cors());
+
+// // Middleware to allow cross-origin requests
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 
 app.get('/', (request, response) => {
