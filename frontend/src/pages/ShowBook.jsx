c4f6f4dbd14cba1b2dc0ec41bb49b0c7e3767b00
Mohamed Zafir Sharaz <zafirsharaz@gmail.com>
2024-03-05 17:35:36 +0530

    Table Design update

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -25,40 +25,48 @@ const ShowBook = () => {
 
   return (
     <div className='p-4'>
-      <BackButton />
-      <h1 className='text-3xl my-4'>Show Book</h1>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Id</span>
-            <span>{book._id}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Title</span>
-            <span>{book.title}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Author</span>
-            <span>{book.author}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
-            <span>{book.publishYear}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Create Time</span>
-            <span>{new Date(book.createdAt).toString()}</span>
-          </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500'>Last Update Time</span>
-            <span>{new Date(book.updatedAt).toString()}</span>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-};
+  <BackButton />
+  <h1 className='text-3xl my-4'>Show Book</h1>
+  {loading ? (
+    <Spinner />
+  ) : (
+    <table className='min-w-full bg-white border border-gray-300 shadow-md rounded-md'>
+      <tbody>
+        {/* Row 1 */}
+        <tr className="bg-gray-100">
+          <td className="py-2 px-4 border-b font-semibold">Id</td>
+          <td className="py-2 px-4 border-b">{book._id}</td>
+        </tr>
+        {/* Row 2 */}
+        <tr className="bg-white">
+          <td className="py-2 px-4 border-b font-semibold">Title</td>
+          <td className="py-2 px-4 border-b">{book.title}</td>
+        </tr>
+        {/* Row 3 */}
+        <tr className="bg-gray-100">
+          <td className="py-2 px-4 border-b font-semibold">Author</td>
+          <td className="py-2 px-4 border-b">{book.author}</td>
+        </tr>
+        {/* Row 4 */}
+        <tr className="bg-white">
+          <td className="py-2 px-4 border-b font-semibold">Publish Year</td>
+          <td className="py-2 px-4 border-b">{book.publishYear}</td>
+        </tr>
+        {/* Row 5 */}
+        <tr className="bg-gray-100">
+          <td className="py-2 px-4 border-b font-semibold">Create Time</td>
+          <td className="py-2 px-4 border-b">{new Date(book.createdAt).toString()}</td>
+        </tr>
+        {/* Row 6 */}
+        <tr className="bg-white">
+          <td className="py-2 px-4 border-b font-semibold">Last Update Time</td>
+          <td className="py-2 px-4 border-b">{new Date(book.updatedAt).toString()}</td>
+        </tr>
+      </tbody>
+    </table>
+  )}
+</div>
+  )
+}
 
 export default ShowBook;
